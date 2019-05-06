9ae5309d2a73c0e92362c13e33720631a8fcada5
Patricio Diaz <pdiaz290@gmail.com>
2019-05-06 16:35:33 -0400

    cambio barra

---
+++ registro/static/js/app.js
@@ -0,0 +1,191 @@
+$(function(){
+    cargarRegion()
+})
+
+
+function cargarRegion(){
+    chile.regiones.forEach(region => {
+        $("#region").append('<option value="'+region.region+'">'+region.region+'</option>')
+    })
+}
+
+$("#region").change(function(){
+    var region = $(this).val()
+    var regionCompleta = chile.regiones.find(r =>r.region == region)
+    $("#comuna").html("")
+    $("#comuna").append('<option hidden ">Seleccione</option>')
+    regionCompleta.comunas.forEach(comuna => {
+       $("#comuna").append('<option value="'+comuna+'">'+comuna+'</option>')
+   })
+    
+})
+
+var chile = {
+    "regiones": [{
+        "region": "Arica y Parinacota",
+        "comunas": ["Arica", "Camarones", "Putre", "General Lagos"]
+    },
+    {
+        "region": "Tarapacá",
+        "comunas": ["Iquique", "Alto Hospicio", "Pozo Almonte", "Camiña", "Colchane", "Huara", "Pica"]
+    },
+    {
+        "region": "Antofagasta",
+        "comunas": ["Antofagasta", "Mejillones", "Sierra Gorda", "Taltal", "Calama", "Ollagüe", "San Pedro de Atacama", "Tocopilla", "María Elena"]
+    },
+    {
+        "region": "Atacama",
+        "comunas": ["Copiapó", "Caldera", "Tierra Amarilla", "Chañaral", "Diego de Almagro", "Vallenar", "Alto del Carmen", "Freirina", "Huasco"]
+    },
+    {
+        "region": "Coquimbo",
+        "comunas": ["La Serena", "Coquimbo", "Andacollo", "La Higuera", "Paiguano", "Vicuña", "Illapel", "Canela", "Los Vilos", "Salamanca", "Ovalle", "Combarbalá", "Monte Patria", "Punitaqui", "Río Hurtado"]
+    },
+    {
+        "region": "Valparaíso",
+        "comunas": ["Valparaíso", "Casablanca", "Concón", "Juan Fernández", "Puchuncaví", "Quintero", "Viña del Mar", "Isla de Pascua", "Los Andes", "Calle Larga", "Rinconada", "San Esteban", "La Ligua", "Cabildo", "Papudo", "Petorca", "Zapallar", "Quillota", "Calera", "Hijuelas", "La Cruz", "Nogales", "San Antonio", "Algarrobo", "Cartagena", "El Quisco", "El Tabo", "Santo Domingo", "San Felipe", "Catemu", "Llaillay", "Panquehue", "Putaendo", "Santa María", "Quilpué", "Limache", "Olmué", "Villa Alemana"]
+    },
+    {
+        "region": "Región del Libertador Gral. Bernardo O’Higgins",
+        "comunas": ["Rancagua", "Codegua", "Coinco", "Coltauco", "Doñihue", "Graneros", "Las Cabras", "Machalí", "Malloa", "Mostazal", "Olivar", "Peumo", "Pichidegua", "Quinta de Tilcoco", "Rengo", "Requínoa", "San Vicente", "Pichilemu", "La Estrella", "Litueche", "Marchihue", "Navidad", "Paredones", "San Fernando", "Chépica", "Chimbarongo", "Lolol", "Nancagua", "Palmilla", "Peralillo", "Placilla", "Pumanque", "Santa Cruz"]
+    },
+    {
+        "region": "Región del Maule",
+        "comunas": ["Talca", "Constitución", "Curepto", "Empedrado", "Maule", "Pelarco", "Pencahue", "Río Claro", "San Clemente", "San Rafael", "Cauquenes", "Chanco", "Pelluhue", "Curicó", "Hualañé", "Licantén", "Molina", "Rauco", "Romeral", "Sagrada Familia", "Teno", "Vichuquén", "Linares", "Colbún", "Longaví", "Parral", "Retiro", "San Javier", "Villa Alegre", "Yerbas Buenas"]
+    },
+    {
+        "region": "Región de Ñuble",
+        "comunas": ["Cobquecura", "Coelemu", "Ninhue", "Portezuelo", "Quirihue", "Ránquil", "Treguaco", "Bulnes", "Chillán Viejo", "Chillán", "El Carmen", "Pemuco", "Pinto", "Quillón", "San Ignacio", "Yungay", "Coihueco", "Ñiquén", "San Carlos", "San Fabián", "San Nicolás"]
+    },
+    {
+        "region": "Región del Biobío",
+        "comunas": ["Concepción", "Coronel", "Chiguayante", "Florida", "Hualqui", "Lota", "Penco", "San Pedro de la Paz", "Santa Juana", "Talcahuano", "Tomé", "Hualpén", "Lebu", "Arauco", "Cañete", "Contulmo", "Curanilahue", "Los Álamos", "Tirúa", "Los Ángeles", "Antuco", "Cabrero", "Laja", "Mulchén", "Nacimiento", "Negrete", "Quilaco", "Quilleco", "San Rosendo", "Santa Bárbara", "Tucapel", "Yumbel", "Alto Biobío"]
+    },
+    {
+        "region": "Región de la Araucanía",
+        "comunas": ["Temuco", "Carahue", "Cunco", "Curarrehue", "Freire", "Galvarino", "Gorbea", "Lautaro", "Loncoche", "Melipeuco", "Nueva Imperial", "Padre las Casas", "Perquenco", "Pitrufquén", "Pucón", "Saavedra", "Teodoro Schmidt", "Toltén", "Vilcún", "Villarrica", "Cholchol", "Angol", "Collipulli", "Curacautín", "Ercilla", "Lonquimay", "Los Sauces", "Lumaco", "Purén", "Renaico", "Traiguén", "Victoria"]
+    },
+    {
+        "region": "Región de Los Ríos",
+        "comunas": ["Valdivia", "Corral", "Lanco", "Los Lagos", "Máfil", "Mariquina", "Paillaco", "Panguipulli", "La Unión", "Futrono", "Lago Ranco", "Río Bueno"]
+    },
+    {
+        "region": "Región de Los Lagos",
+        "comunas": ["Puerto Montt", "Calbuco", "Cochamó", "Fresia", "Frutillar", "Los Muermos", "Llanquihue", "Maullín", "Puerto Varas", "Castro", "Ancud", "Chonchi", "Curaco de Vélez", "Dalcahue", "Puqueldón", "Queilén", "Quellón", "Quemchi", "Quinchao", "Osorno", "Puerto Octay", "Purranque", "Puyehue", "Río Negro", "San Juan de la Costa", "San Pablo", "Chaitén", "Futaleufú", "Hualaihué", "Palena"]
+    },
+    {
+        "region": "Región Aisén del Gral. Carlos Ibáñez del Campo",
+        "comunas": ["Coihaique", "Lago Verde", "Aisén", "Cisnes", "Guaitecas", "Cochrane", "O’Higgins", "Tortel", "Chile Chico", "Río Ibáñez"]
+    },
+    {
+        "region": "Región de Magallanes y de la Antártica Chilena",
+        "comunas": ["Punta Arenas", "Laguna Blanca", "Río Verde", "San Gregorio", "Cabo de Hornos (Ex Navarino)", "Antártica", "Porvenir", "Primavera", "Timaukel", "Natales", "Torres del Paine"]
+    },
+    {
+        "region": "Región Metropolitana de Santiago",
+        "comunas": ["Cerrillos", "Cerro Navia", "Conchalí", "El Bosque", "Estación Central", "Huechuraba", "Independencia", "La Cisterna", "La Florida", "La Granja", "La Pintana", "La Reina", "Las Condes", "Lo Barnechea", "Lo Espejo", "Lo Prado", "Macul", "Maipú", "Ñuñoa", "Pedro Aguirre Cerda", "Peñalolén", "Providencia", "Pudahuel", "Quilicura", "Quinta Normal", "Recoleta", "Renca", "San Joaquín", "San Miguel", "San Ramón", "Vitacura", "Puente Alto", "Pirque", "San José de Maipo", "Colina", "Lampa", "Tiltil", "San Bernardo", "Buin", "Calera de Tango", "Paine", "Melipilla", "Alhué", "Curacaví", "María Pinto", "San Pedro", "Talagante", "El Monte", "Isla de Maipo", "Padre Hurtado", "Peñaflor"]
+        }]
+    }
+
+
+
+function checkRut(rut) {
+
+     // Despejar Puntos
+     var valor = rut.value.replace('.','');
+     // Despejar Guión
+     valor = valor.replace('-','');
+    
+     // Aislar Cuerpo y Dígito Verificador
+     cuerpo = valor.slice(0,-1);
+     dv = valor.slice(-1).toUpperCase();
+    
+     // Formatear RUN
+     rut.value = cuerpo + '-'+ dv
+    
+     // Si no cumple con el mínimo ej. (n.nnn.nnn)
+     if(cuerpo.length < 7) { rut.setCustomValidity("RUT Incompleto"); return false;}
+    
+     // Calcular Dígito Verificador
+     suma = 0;
+     multiplo = 2;
+    
+     // Para cada dígito del Cuerpo
+     for(i=1;i<=cuerpo.length;i++) {
+    
+         // Obtener su Producto con el Múltiplo Correspondiente
+         index = multiplo * valor.charAt(cuerpo.length - i);
+        
+         // Sumar al Contador General
+         suma = suma + index;
+        
+         // Consolidar Múltiplo dentro del rango [2,7]
+         if(multiplo < 7) { multiplo = multiplo + 1; } else { multiplo = 2; }
+  
+     }
+    
+     // Calcular Dígito Verificador en base al Módulo 11
+     dvEsperado = 11 - (suma % 11);
+    
+     // Casos Especiales (0 y K)
+     dv = (dv == 'K')?10:dv;
+     dv = (dv == 0)?11:dv;
+    
+     // Validar que el Cuerpo coincide con su Dígito Verificador
+     if(dvEsperado != dv) { rut.setCustomValidity("RUT Inválido"); return false; }
+    
+     // Si todo sale bien, eliminar errores (decretar que es válido)
+     rut.setCustomValidity('');
+ }
+
+ $("#form1").validate({
+    rules: {
+        pass1: { 
+          required: true,
+             minlength: 6,
+             maxlength: 10,
+
+        } , 
+
+            pass2: { 
+             equalTo: "#pass1",
+              minlength: 6,
+              maxlength: 10
+        }
+
+
+    },
+messages:{
+  pass1: { 
+          required:"Password Requerido",
+          minlength: "Minimo 6 caracteres",
+          maxlength: "Maximo 10 caracteres"
+        },
+pass2: { 
+  equalTo: "El password debe ser igual al anterior",
+  minlength: "Minimo 6 caracteres",
+  maxlength: "Maximo 10 caracteres"
+}
+}
+
+});
+
+
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/registro/templates/sw.js')
+        .then(function() {
+            console.log('ServiceWorker registered!');
+        })
+        .catch(function(err) {
+            console.log('ServiceWorker failed :(', err);
+        });
+    });
+  }
+
+
+  $(document).ready(main);
+
+var contador = 1;
+
